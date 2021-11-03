59570be154da8f12af293d9fe661710dcf5aff13
Jeffrey Kirchner <jkirchner@gmail.com>
2021-11-02 22:13:05 -0700

    transfers

--- main/templates/staff/staff_session/graph_card.js
+++ main/templates/staff/staff_session/graph_card.js
@@ -84,165 +84,172 @@ setupPixiPlayers(){
     //setup pixi houses
     for(let i=0;i<session_players.length;i++)
     {
-        let container = new PIXI.Container();
-
-        pt = app.getLocationCordinates(session_players[i].parameter_set_player.location, 'house');
-
-        session_player = session_players[i];
-        parameter_set_player = session_player.parameter_set_player;
-        parameter_set = app.$data.session.parameter_set;
-
-        //house texture
-        let sprite = PIXI.Sprite.from(app.$data.house_sheet.textures["House0000"]);
-
-        sprite.x = 0;
-        sprite.y = 0;
-        sprite.name = "house_texture"
-        sprite.interactive = true;
-        sprite.buttonMode = true;
-        sprite.tint = 0xD3D3D3;
-        sprite.on('pointerdown', (event) => { app.handleHousePointerDown(i) });
-        sprite.on('pointerup', (event) => { app.handleHousePointerUp(i) });
-
-        container.addChild(sprite)
-
-        //highlight
-        let highlight = new PIXI.Graphics();
-        highlight.beginFill(0xD5402B);
-        highlight.drawRoundedRect(-sprite.width * 0.01, -sprite.height * 0.01, sprite.width + sprite.width * 0.02, sprite.height + sprite.height * 0.02, 20);
-        highlight.name = "highlight";
-        highlight.endFill();
-        highlight.visible=false;
-        container.addChildAt(highlight, 0)
-
-        //house label texture
-        let label = new PIXI.Text(parameter_set_player.id_label,{fontFamily : 'Arial',
-                                                                      fontWeight:'bold',
-                                                                      fontSize: 48,
-                                                                      align : 'center'});
-        label.anchor.set(0.5);
-        label.x = sprite.width / 2;
-        label.y = 50;
-        label.name = "id_label"
-
-        container.addChild(label);
-
-        //good one label
-        let goodOneLabel = new PIXI.Text(session_player.good_one_house.toString(),{fontFamily : 'Arial',
-                                                                              fontWeight:'bold',
-                                                                              fontSize: 100,
-                                                                              fill: parameter_set.good_one_rgb_color,
-                                                                              align : 'center'});
-
-        goodOneLabel.anchor.set(0.5);
-        goodOneLabel.x = sprite.width / 2;
-        goodOneLabel.y = 185;
-        goodOneLabel.name = "good_one_label"
-
-        container.addChild(goodOneLabel)
-
-        //good one label
-        let goodTwoLabel = new PIXI.Text(session_player.good_two_house.toString(),{fontFamily : 'Arial',
-                                                                                        fontWeight:'bold',
-                                                                                        fontSize: 100,
-                                                                                        fill: parameter_set.good_two_rgb_color,
-                                                                                        align : 'center'});
-
-        goodTwoLabel.anchor.set(0.5);
-        goodTwoLabel.x = sprite.width / 2;
-        goodTwoLabel.y = 300;
-        goodTwoLabel.name = "good_two_label"
-
-        container.addChild(goodTwoLabel)
-
-        container.x = pt.x;
-        container.y = pt.y;
-        container.pivot.set(container.width/2, container.height/2);
-        container.scale.set(app.$data.canvas_scale, app.$data.canvas_scale);
-    
-        session_players[i].houseContainer = container;
-        app.$data.pixi_app.stage.addChild(session_players[i].houseContainer);
-
+        app.setupSingleHoue(i);
     }
 
     //setup pixi fields
     for(let i=0;i<session_players.length;i++)
     {
-        let container = new PIXI.Container();
-
-        pt = app.getLocationCordinates(session_players[i].parameter_set_player.location, 'field');
-
-        session_player = session_players[i];
-        parameter_set_player = session_player.parameter_set_player;
-        parameter_set = app.$data.session.parameter_set;
-
-        //house texture
-        let sprite = PIXI.Sprite.from(app.$data.house_sheet.textures["Field0000"]);
-
-        sprite.x = 0;
-        sprite.y = 0;
-        sprite.tint = 0xD3D3D3;
-        sprite.interactive = true;
-        sprite.buttonMode = true;
-        sprite.on('pointerdown', (event) => { app.handleFieldPointerDown(i) });
-        sprite.on('pointerup', (event) => { app.handleFieldPointerUp(i) });
-        
-        container.addChild(sprite)
-
-        //house label texture
-        let label = new PIXI.Text(parameter_set_player.id_label,{fontFamily : 'Arial',
-                                                                      fontWeight:'bold',
-                                                                      fontSize: 48,
-                                                                      align : 'center'});
-        label.anchor.set(0.5);
-        label.x = sprite.width - 25;
-        label.y = 35;
-
-        container.addChild(label);
-
-        //good one label
-        let goodOneLabel = new PIXI.Text(session_player.good_one_field.toString(),{fontFamily : 'Arial',
-                                                                              fontWeight:'bold',
-                                                                              fontSize: 100,
-                                                                              fill: parameter_set.good_one_rgb_color,
-                                                                              align : 'center'});
-
-        goodOneLabel.anchor.set(0.5);
-        goodOneLabel.x = sprite.width / 2;
-        goodOneLabel.y = sprite.height / 4;
-
-        container.addChild(goodOneLabel)
-
-        //good one label
-        let goodTwoLabel = new PIXI.Text(session_player.good_two_field.toString(),{fontFamily : 'Arial',
-                                                                                        fontWeight:'bold',
-                                                                                        fontSize: 100,
-                                                                                        fill: parameter_set.good_two_rgb_color,
-                                                                                        align : 'center'});
-
-        goodTwoLabel.anchor.set(0.5);
-        goodTwoLabel.x = sprite.width / 2;
-        goodTwoLabel.y = sprite.height / 4 * 3;
-
-        container.addChild(goodTwoLabel)
-
-        container.x = pt.x;
-        container.y = pt.y;
-        container.pivot.set(container.width/2, container.height/2);
-        container.scale.set(app.$data.canvas_scale, app.$data.canvas_scale);    
-        
-        // container.interactive=true
-        // container.on('pointerenter', (event) => { app.handleFieldPointerEnter(i) });
-        // container.on('pointerleave', (event) => { app.handleFieldPointerLeave(i) });
-        // container.hitArea = new PIXI.Rectangle(0, 0, container.width, container.height);
-
-        session_players[i].fieldContainer = container;
-        app.$data.pixi_app.stage.addChild(session_players[i].fieldContainer);
+        app.setupSingleField(i);
     }
 },
 
-setupSingleHoue(){
+setupSingleHoue(index){
+    let container = new PIXI.Container();
 
+    pt = app.getLocationCordinates(session_players[index].parameter_set_player.location, 'house');
+
+    session_player = session_players[index];
+    parameter_set_player = session_player.parameter_set_player;
+    parameter_set = app.$data.session.parameter_set;
+
+    //house texture
+    let sprite = PIXI.Sprite.from(app.$data.house_sheet.textures["House0000"]);
+
+    sprite.x = 0;
+    sprite.y = 0;
+    sprite.name = "house_texture"
+    sprite.interactive = true;
+    sprite.buttonMode = true;
+    sprite.tint = 0xD3D3D3;
+    sprite.on('pointerdown', (event) => { app.handleHousePointerDown(index) });
+    sprite.on('pointerup', (event) => { app.handleHousePointerUp(index) });
+
+    container.addChild(sprite)
+
+    //highlight
+    let highlight = new PIXI.Graphics();
+    highlight.beginFill(0xD5402B);
+    highlight.drawRoundedRect(-sprite.width * 0.01, -sprite.height * 0.01, sprite.width + sprite.width * 0.02, sprite.height + sprite.height * 0.02, 20);
+    highlight.name = "highlight";
+    highlight.endFill();
+    highlight.visible=false;
+    container.addChildAt(highlight, 0)
+
+    //house label texture
+    let label = new PIXI.Text(parameter_set_player.id_label,{fontFamily : 'Arial',
+                                                                    fontWeight:'bold',
+                                                                    fontSize: 48,
+                                                                    align : 'center'});
+    label.anchor.set(0.5);
+    label.x = sprite.width / 2;
+    label.y = 50;
+    label.name = "id_label"
+
+    container.addChild(label);
+
+    //good one label
+    let goodOneLabel = new PIXI.Text(session_player.good_one_house.toString(),{fontFamily : 'Arial',
+                                                                            fontWeight:'bold',
+                                                                            fontSize: 100,
+                                                                            fill: parameter_set.good_one_rgb_color,
+                                                                            align : 'center'});
+
+    goodOneLabel.anchor.set(0.5);
+    goodOneLabel.x = sprite.width / 2;
+    goodOneLabel.y = 185;
+    goodOneLabel.name = "good_one_label"
+
+    container.addChild(goodOneLabel)
+
+    //good one label
+    let goodTwoLabel = new PIXI.Text(session_player.good_two_house.toString(),{fontFamily : 'Arial',
+                                                                                    fontWeight:'bold',
+                                                                                    fontSize: 100,
+                                                                                    fill: parameter_set.good_two_rgb_color,
+                                                                                    align : 'center'});
+
+    goodTwoLabel.anchor.set(0.5);
+    goodTwoLabel.x = sprite.width / 2;
+    goodTwoLabel.y = 300;
+    goodTwoLabel.name = "good_two_label"
+
+    container.addChild(goodTwoLabel)
+
+    container.x = pt.x;
+    container.y = pt.y;
+    container.pivot.set(container.width/2, container.height/2);
+    container.scale.set(app.$data.canvas_scale, app.$data.canvas_scale);
+
+    session_players[index].houseContainer = container;
+    app.$data.pixi_app.stage.addChild(session_players[index].houseContainer);
+},
+
+setupSingleField(index){
+    let container = new PIXI.Container();
+
+    pt = app.getLocationCordinates(session_players[index].parameter_set_player.location, 'field');
+
+    session_player = session_players[index];
+    parameter_set_player = session_player.parameter_set_player;
+    parameter_set = app.$data.session.parameter_set;
+
+    //house texture
+    let sprite = PIXI.Sprite.from(app.$data.house_sheet.textures["Field0000"]);
+
+    sprite.x = 0;
+    sprite.y = 0;
+    sprite.tint = 0xD3D3D3;
+    //sprite.interactive = true;
+    //sprite.buttonMode = true;
+    //sprite.on('pointerdown', (event) => { app.handleFieldPointerDown(i) });
+    //sprite.on('pointerup', (event) => { app.handleFieldPointerUp(i) });
+    //sprite.on('pointerup', (event) => { app.handleFieldPointerUp(i) });
+    
+    container.addChild(sprite)
+
+    //house label texture
+    let label = new PIXI.Text(parameter_set_player.id_label,{fontFamily : 'Arial',
+                                                                    fontWeight:'bold',
+                                                                    fontSize: 48,
+                                                                    align : 'center'});
+    label.anchor.set(0.5);
+    label.x = sprite.width - 25;
+    label.y = 35;
+
+    container.addChild(label);
+
+    //good one label
+    let goodOneLabel = new PIXI.Text(session_player.good_one_field.toString(),{fontFamily : 'Arial',
+                                                                            fontWeight:'bold',
+                                                                            fontSize: 100,
+                                                                            fill: parameter_set.good_one_rgb_color,
+                                                                            align : 'center'});
+
+    goodOneLabel.anchor.set(0.5);
+    goodOneLabel.x = sprite.width / 2;
+    goodOneLabel.y = sprite.height / 4;
+
+    container.addChild(goodOneLabel)
+
+    //good one label
+    let goodTwoLabel = new PIXI.Text(session_player.good_two_field.toString(),{fontFamily : 'Arial',
+                                                                                    fontWeight:'bold',
+                                                                                    fontSize: 100,
+                                                                                    fill: parameter_set.good_two_rgb_color,
+                                                                                    align : 'center'});
+
+    goodTwoLabel.anchor.set(0.5);
+    goodTwoLabel.x = sprite.width / 2;
+    goodTwoLabel.y = sprite.height / 4 * 3;
+
+    container.addChild(goodTwoLabel)
+
+    container.x = pt.x;
+    container.y = pt.y;
+    container.pivot.set(container.width/2, container.height/2);
+    container.hitArea = new PIXI.Rectangle(0, 0, container.width, container.height);    
+    
+    container.interactive=true
+    container.buttonMode = true;
+    container.on('pointerdown', (event) => { app.handleFieldPointerDown(index) });
+    container.on('pointerup', (event) => { app.handleFieldPointerUp(index) });
+    //container.on('pointermove', (event) => { app.handleFieldPointerEnter(i) });
+    
+    container.scale.set(app.$data.canvas_scale, app.$data.canvas_scale);
+
+    session_players[index].fieldContainer = container;
+    app.$data.pixi_app.stage.addChild(session_players[index].fieldContainer);
 },
 
 /**
@@ -312,6 +319,7 @@ getLocationCordinates(index, field_or_house){
  */
 handleFieldPointerDown(index){
     console.log('Field ' + (index+1).toString() + ' down');
+    app.turnOffHighlights();
 },
 
 /**
@@ -334,10 +342,11 @@ handleFieldPointerLeave(index){
  */
 handleHousePointerDown(index){
     console.log('House ' + (index+1).toString() + ' down');
+    app.turnOffHighlights();
+
     session_players[index].houseContainer.getChildByName("highlight").visible=true;
     app.$data.pixi_transfer_source = session_players[index].houseContainer;
-    app.updatePixiTransfer(event.offsetX , event.offsetY)
-    
+    app.updatePixiTransfer(event.offsetX , event.offsetY);    
 },
 
 /**
@@ -352,11 +361,16 @@ handleHousePointerDown(index){
  */
  handleStagePointerUp(){
     console.log('Stage up: ' + event);
-    app.$data.pixi_transfer_line.visible=false;
+    app.turnOffHighlights();
+},
+
+turnOffHighlights(){
     for(let i=0;i<session_players.length;i++)
     {
         session_players[i].houseContainer.getChildByName("highlight").visible=false;
     }
+
+    app.$data.pixi_transfer_line.visible=false;
 },
 
 /**
@@ -375,7 +389,7 @@ updatePixiTransfer(target_x, target_y){
 
     transfer_line.clear();
 
-    transfer_line.lineStyle({width:10, color:0xD5402B, alpha:1, alignment:0.5, cap:PIXI.LINE_CAP.ROUND});
+    transfer_line.lineStyle({width:10, color:0xD5402B, alpha:1, alignment:0.5, cap:PIXI.LINE_CAP.SQUARE});
     transfer_line.visible=true;
     transfer_line.moveTo(target_x, target_y);
     transfer_line.lineTo(source.x, source.y);
