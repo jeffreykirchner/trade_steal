d28d1f79fc2972083f6719de39d765f7b4d4af76
Jeffrey Kirchner <jkirchner@gmail.com>
2021-11-04 15:04:04 -0700

    transfers

---
+++ main/forms/session_player_move_form.py
@@ -0,0 +1,23 @@
+'''
+session player move form
+'''
+
+from django import forms
+
+from main.models import SessionPlayerMove
+
+class SessionForm(forms.Form):
+    '''
+    session player move form
+    '''
+    good_one_amount = forms.IntegerField(label='Good One Amount',
+                                         min_value=0,
+                                         min_value=9999,
+                                         widget=forms.NumberInput(attrs={"step":"1",
+                                                                         "min":"0"}))
+    
+    good_two_amount = forms.IntegerField(label='Good Two Amount',
+                                         min_value=0,
+                                         min_value=9999,
+                                         widget=forms.NumberInput(attrs={"step":"1",
+                                                                         "min":"0"}))
--- main/models/session_player_move.py
+++ main/models/session_player_move.py
@@ -5,16 +5,22 @@ session move goods
 #import logging
 
 from django.db import models
+from django.db.models import Q
+from django.db.models import F
 
 from main.models import SessionPlayer
+from main.models import SessionPeriod
+
 from main.globals import ContainerTypes
 
 class SessionPlayerMove(models.Model):
     '''
     session player move model
     '''
-    session_player_source = models.ForeignKey(SessionPlayer, on_delete=models.CASCADE, related_name="session_player_move_sources")
-    session_player_target = models.ForeignKey(SessionPlayer, on_delete=models.CASCADE, related_name="session_player_move_targets")
+    session_period = models.ForeignKey(SessionPeriod, on_delete=models.CASCADE, related_name="session_player_move_a")
+
+    session_player_source = models.ForeignKey(SessionPlayer, on_delete=models.CASCADE, related_name="session_player_move_b")
+    session_player_target = models.ForeignKey(SessionPlayer, on_delete=models.CASCADE, related_name="session_player_move_c")
 
     good_one_amount = models.IntegerField(verbose_name='Good one amount')        #amount of good one to be moved
     good_two_amount = models.IntegerField(verbose_name='Good two amount')        #amount of good two to be moved
@@ -33,6 +39,12 @@ class SessionPlayerMove(models.Model):
         verbose_name = 'Session Player Move'
         verbose_name_plural = 'Session Player Moves'
         ordering = ['timestamp']
+        constraints = [
+            models.CheckConstraint(check=Q(good_one_amount__gte=0), name='good_one_amount__gte_0'),
+            models.CheckConstraint(check=Q(good_two_amount__gte=0), name='good_two_amount__gte_0'),
+            models.CheckConstraint(check=~Q(session_player_source=F('session_player_target')) &
+                                         ~Q(source_container=F('target_container')), name='source_equals_target'),
+        ]
 
     #return json object of class
     def json(self):
