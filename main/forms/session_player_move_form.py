d208ed9effcfeee8394d3f8dc95091a026a8afb7
Jeffrey Kirchner <jkirchner@gmail.com>
2021-11-08 10:53:00 -0800

    transfers

--- main/forms/session_player_move_form.py
+++ main/forms/session_player_move_form.py
@@ -4,13 +4,15 @@ session player move form
 
 from django import forms
 
+from django.utils.safestring import mark_safe
+
 from main.models import SessionPlayerMove
 
 class SessionPlayerMoveForm(forms.Form):
     '''
     session player move form
     '''
-    good_one_amount = forms.IntegerField(label='Good One Amount',
+    good_one_amount = forms.IntegerField(label=mark_safe('<span v-bind:style = "{color : transfer_modal_good_one_rgb}">  [[transfer_modal_good_one_name]]</span> Amount'),
                                          min_value=0,
                                          max_value=9999,
                                          widget=forms.NumberInput(attrs={"step" : "1",
