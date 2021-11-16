192cb743754829c1c829ae3ddd78c0838582928e
Jeffrey Kirchner <kirchner@chapman.edu>
2021-11-16 14:40:23 -0800

    update test setup

--- scripts/test_setup.py
+++ scripts/test_setup.py
@@ -4,8 +4,8 @@ session_id = input()
 
 from main.models import Session
 session = Session.objects.get(id=session_id)
-session.session_players.all().update(good_one_house=888)
-session.session_players.all().update(good_two_house=888)
-session.session_players.all().update(good_three_house=888)
-session.session_players.all().update(good_one_field=888)
-session.session_players.all().update(good_two_field=888)
+session.session_players.all().update(good_one_house=25)
+session.session_players.all().update(good_two_house=25)
+session.session_players.all().update(good_three_house=25)
+session.session_players.all().update(good_one_field=25)
+session.session_players.all().update(good_two_field=25)
