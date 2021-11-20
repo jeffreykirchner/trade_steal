64e0722819b89961c0e45c23f815120570794ff7
Jeffrey Kirchner <jkirchner@gmail.com>
2021-11-19 21:33:40 -0800

    add auth portal login

--- main/views/registration/login_view.py
+++ main/views/registration/login_view.py
@@ -3,15 +3,17 @@ log in user functionality
 '''
 import json
 import logging
+import requests
 
 from django.contrib.auth import authenticate, login,logout
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from main.models import Parameters
 from main.forms import LoginForm
-
 
 class LoginView(TemplateView):
     '''
@@ -27,7 +29,7 @@ class LoginView(TemplateView):
         data = json.loads(request.body.decode('utf-8'))
 
         if data["action"] == "login":
-            return login_function(request,data)
+            return login_function(request, data)
 
         return JsonResponse({"response" :  "error"},safe=False)
 
@@ -73,7 +75,10 @@ def login_function(request,data):
 
         #logger.info(f"Login user {username}")
 
-        user = authenticate(request, username=username.lower(), password=password)
+        user = login_function_esi_auth(username=username.lower(), password=password)
+
+        if not user:
+            user = authenticate(request, username=username.lower(), password=password)
 
         if user is not None:
             login(request, user)
@@ -90,3 +95,71 @@ def login_function(request,data):
     else:
         logger.info("Login user form validation error")
         return JsonResponse({"status":"validation", "errors":dict(form.errors.items())}, safe=False)
+
+def login_function_esi_auth(username, password):
+    '''
+    check the esi auth server for account access
+    return user if already exists or create new one.
+    
+    username: string, user name of account to login
+    password: string, password of account to login
+    '''
+
+    logger = logging.getLogger(__name__)
+
+    try:
+        headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json'}
+
+        data = {"app_name" : settings.ESI_AUTH_APP,
+                "username" : username,
+                "password" :password }
+
+        request_result = requests.get(f'{settings.ESI_AUTH_URL}/get-auth/',
+                                    json=data,
+                                    auth=(str(settings.ESI_AUTH_USERNAME), str(settings.ESI_AUTH_PASS)),
+                                    headers=headers)
+        
+        if request_result.status_code != 200:        
+            logger.warning(f'ESI auth error: {request_result}')
+            return None
+
+        request_result_json = request_result.json()
+
+        if request_result_json['status'] == 'fail':        
+            logger.warning(f'ESI auth error: {request_result}')
+            return None
+
+        logger.info(f"ESI auth response: {request_result_json}")
+
+        profile = request_result_json['profile']
+        
+        users = get_user_model().objects.filter(username=profile['global_id'])
+
+        if users.count() == 0:
+            #check for duplicate email
+            if get_user_model().objects.filter(email=profile['email']).count()>0:
+                logger.warning(f"ESI auth response: email already exists {profile['email']}")
+                return None
+
+            user = get_user_model().objects.create_user(username=profile['global_id'],
+                                                email=profile['email'],
+                                                password=password,                                         
+                                                first_name=profile['first_name'],
+                                                last_name=profile['last_name'])
+            
+            logger.info(f"ESI auth user not found, create new user: {user}")
+        else:
+            user = users.first()
+            user.email = profile['email']
+            user.password = password
+            user.first_name = profile['first_name']
+            user.last_name = profile['last_name']
+
+            user.save()
+
+            logger.info(f"ESI auth user found, update user: {user}")
+
+        return user
+    except Exception  as e: 
+        logger.warning(f'ESI auth error: user {username} failed to connect to auth server. {e}')
+        return None
--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
+python manage.py migrate
 daphne -b 0.0.0.0 _trade_steal.asgi:application
