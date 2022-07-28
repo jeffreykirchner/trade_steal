9f0de73fe82e40ca169b58b5e47c02b7247702aa
Jeffrey Kirchner <jkirchner@gmail.com>
2022-07-28 11:11:28 -0700

    Pressing enter on subject name / id entry works. Fixes #109.

--- main/forms/end_game_form.py
+++ main/forms/end_game_form.py
@@ -8,5 +8,9 @@ class EndGameForm(forms.Form):
     '''
     end game form
     '''
-    name =  forms.CharField(label='Enter your full name')
-    student_id =  forms.CharField(label='Student ID', required=False)
+    name =  forms.CharField(label='Enter your full name',
+                            widget=forms.TextInput(attrs={"v-on:keyup.enter":"sendName()"}))
+
+    student_id =  forms.CharField(label='Student ID',
+                                  required=False,
+                                  widget=forms.TextInput(attrs={"v-on:keyup.enter":"sendName()"}))
