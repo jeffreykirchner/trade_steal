340e4af84811ca600822ba79de9ea78f1c258b89
Jeffrey Kirchner <kirchner@chapman.edu>
2023-04-04 12:34:15 -0700

    remove prolific_study_id

--- main/forms/parameter_set_form.py
+++ main/forms/parameter_set_form.py
@@ -139,20 +139,6 @@ class ParameterSetForm(forms.ModelForm):
 
         return survey_link
 
-    def clean_prolific_study_id(self):
-        
-        try:
-           prolific_study_id = self.data.get('prolific_study_id')
-           prolific_mode = self.data.get('prolific_mode')
-
-           if prolific_mode == 'True' and (not prolific_study_id or prolific_study_id ==""):
-               raise forms.ValidationError('Enter Prolific Study ID')
-            
-        except ValueError:
-            raise forms.ValidationError('Invalid Entry')
-
-        return prolific_study_id
-
     def clean_post_forward_link(self):
         
         try:
