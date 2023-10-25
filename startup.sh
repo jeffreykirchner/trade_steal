76f7ab9d412c510904613227e006e3256c8fd9db
Jeffrey Kirchner <jkirchner@gmail.com>
2023-10-25 13:07:14 -0700

    move redis to local install.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 echo "*** Startup.sh ***"
 echo "Run Migrations:"
 python manage.py migrate
-# echo "Install htop:"
-# apt-get -y install htop
+echo "Install htop:"
+apt-get -y install htop
+echo "Install redis"
+apt-get -y install redis
 echo "Start Daphne:"
-daphne -b 0.0.0.0 _trade_steal.asgi:application
+redis-server & daphne -b 0.0.0.0 _trade_steal.asgi:application
