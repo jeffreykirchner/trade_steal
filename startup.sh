f261206ccb00487aba932bb6522495a72245ee43
Jeffrey Kirchner <jkirchner@gmail.com>
2024-12-03 17:36:10 +0000

    fix: remove unnecessary sysctl commands from startup script

--- startup.sh
+++ startup.sh
@@ -5,8 +5,6 @@ python manage.py migrate
 echo "Install htop:"
 apt-get -y install htop
 echo "Install redis"
-sysctl vm.overcommit_memory=1
-echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
 apt-get -y install redis
 echo "Start Server:"
 redis-server & daphne -b 0.0.0.0 _trade_steal.asgi:application
