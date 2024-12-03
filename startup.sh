8bbe16fef3a4812fef75c2d84aa78668a72b75ab
Jeffrey Kirchner <jkirchner@gmail.com>
2024-12-03 17:23:54 +0000

    fix: update ASGI application reference in startup script

--- startup.sh
+++ startup.sh
@@ -5,7 +5,8 @@ python manage.py migrate
 echo "Install htop:"
 apt-get -y install htop
 echo "Install redis"
-apt-get -y install redis
 sysctl vm.overcommit_memory=1
+echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
+apt-get -y install redis
 echo "Start Server:"
-redis-server & daphne -b 0.0.0.0 _multi_user_socket_template.asgi:application
+redis-server & daphne -b 0.0.0.0 _trade_steal.asgi:application
