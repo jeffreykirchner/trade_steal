94412f97579137319e87e12958bf493b8fc11964
Jeffrey Kirchner <jkirchner@gmail.com>
2026-02-19 15:42:10 -0800

    fix: update dependencies in requirements.txt and enhance setup scripts for database and superuser creation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
-echo "setup trade_steal"
+echo "Setup Trade Steal environment"
 sudo service postgresql restart
-echo "drop trade steal db: enter db password"
-dropdb trade_steal -U dbadmin -h localhost -i -p 5433
-echo "create database: enter db password"
-createdb -h localhost -p 5433 -U dbadmin -O dbadmin trade_steal
-echo "restore database: enter db password"
-pg_restore -v --no-owner --role=dbowner --host=localhost --port=5433 --username=dbadmin --dbname=trade_steal database_dumps/trade_steal.sql
+sudo service redis-server start
+echo "Drop Trade Steal db: enter db password"
+dropdb trade_steal -U dbadmin -h localhost -i -p 5432
+echo "Create database: enter db password"
+createdb -h localhost -p 5432 -U dbadmin -O dbadmin trade_steal
+echo "Restore database? (y/n)"
+read restore
+if [ "$restore" = "y" ]; then
+    echo "Restore database: enter db password"
+    pg_restore -v --no-owner --role=dbowner --host=localhost --port=5432 --username=dbadmin --dbname=trade_steal database_dumps/trade_steal.sql
+else
+    python manage.py migrate
+    echo "Create Super User:"
+    python manage.py setup_superuser_with_profile
+    python manage.py setup_site_parameters    
+fi
+echo "Setup complete."
